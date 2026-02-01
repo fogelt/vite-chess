2594e37131edd71613975ef2c9ba273f79fa86a6
fogelt <fogeltt@gmail.com>
2026-02-01 15:04:57 +0100

    added call to start game / refresh board

--- src/components/layouts/game-layout.tsx
+++ src/components/layouts/game-layout.tsx
@@ -1,16 +1,27 @@
 import { ChessBoard, PrimaryContainer, PrimaryButton } from "@/components/ui"
 import { useNavigate } from "react-router-dom";
-import { ChessQueen } from "lucide-react";
+import { ChessQueen, ChessBishop } from "lucide-react";
+import { useStart } from "@/services";
+import { useState } from "react";
 
 export function GameLayout() {
   const navigate = useNavigate();
+  const [gameId, setGameId] = useState(0);
+  const { startGame } = useStart();
+
+  const handleStart = async () => {
+    await startGame();
+    setGameId(prev => prev + 1); // Changing this forces ChessBoard to "reload"
+  };
+
   return (
     <div className="flex p-4 ml-[30%] min-h-screen gap-5 items-center justify-center">
       <PrimaryContainer>
-        <PrimaryButton className="flex" onClick={() => navigate('/')}><ChessQueen size={25} className="mt-1 mr-5" />Go back</PrimaryButton>
+        <PrimaryButton className="flex" onClick={handleStart}><ChessQueen size={25} className="mt-1 mr-5" />Start</PrimaryButton>
+        <PrimaryButton className="flex" onClick={() => navigate('/')}><ChessBishop size={25} className="mt-1 mr-5" />Go back</PrimaryButton>
       </PrimaryContainer>
       <PrimaryContainer>
-        <ChessBoard />
+        <ChessBoard key={gameId} />
       </PrimaryContainer>
     </div>
   );
--- src/services/board-service/board-service.ts
+++ src/services/board-service/board-service.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 const BOARD_API = import.meta.env.VITE_GET_BOARD_API
+const START_API = import.meta.env.VITE_START_GAME_API
 
 interface ChessPiece {
   key: {
@@ -11,13 +12,35 @@ interface ChessPiece {
   color: string | null;
 }
 
+export function useStart() {
+  const startGame = async () => {
+    try {
+      const response = await fetch(START_API, {
+        method: "POST"
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to initialize game");
+      }
+
+    } catch (error) {
+      console.error("Could not post start:", error);
+      throw error;
+    }
+  };
+
+  return { startGame };
+}
+
 export function useBoard() {
   const [board, setBoard] = useState<ChessPiece[]>([]);
 
   useEffect(() => {
     async function fetchPiece() {
       try {
-        const response = await fetch(BOARD_API);
+        const response = await fetch(BOARD_API, {
+          cache: 'no-store'
+        });
 
         if (!response.ok) {
           throw new Error("Failed to fetch piece");
