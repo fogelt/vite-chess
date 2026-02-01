f1021ad9e77a9daf53b65e1a94ad927f67280e79
fogelt <fogeltt@gmail.com>
2026-02-01 01:45:47 +0100

    updated styling

--- src/components/layouts/landing-layout.tsx
+++ src/components/layouts/landing-layout.tsx
@@ -1,9 +1,13 @@
-import { PrimaryButton } from "@/components/ui"
+import { PrimaryButton, PrimaryContainer } from "@/components/ui"
+import { ChessQueen, ChessBishop } from "lucide-react";
 
 export function LandingLayout() {
   return (
     <div className="flex p-4 ml-[30%] min-h-screen items-center justify-center">
-      <PrimaryButton className="">Play</PrimaryButton>
+      <PrimaryContainer>
+        <PrimaryButton className="flex"><ChessQueen size={25} className="mt-1 mr-5" />Play random</PrimaryButton>
+        <PrimaryButton className="flex line-through"><ChessBishop size={25} className="mt-1 mr-5" />New Lobby</PrimaryButton>
+      </PrimaryContainer>
     </div>
   );
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export * from './primary-button/primary-button';
+export * from './primary-container/primary-container';
