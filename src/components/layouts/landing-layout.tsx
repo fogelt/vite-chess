d5d4764c5421023484d6805b359928fd4ef6d842
fogelt <fogeltt@gmail.com>
2026-02-03 20:54:11 +0100

    polished landing page ui and components

--- src/components/layouts/landing-layout.tsx
+++ src/components/layouts/landing-layout.tsx
@@ -5,20 +5,39 @@ import { useNavigate } from "react-router-dom";
 export function LandingLayout() {
   const navigate = useNavigate();
 
+  const wrapperClass = "animate-in fade-in slide-in-from-bottom-24 duration-700 fill-mode-backwards";
+  const containerClass = "flex flex-col items-center justify-center gap-5 transition-transform duration-200 hover:scale-[1.05]";
+
   return (
     <div className="flex min-h-screen items-center justify-center ml-[20%] gap-10">
-      <PrimaryContainer className="flex flex-col items-center justify-center gap-5 hover:scale-[1.05] duration-200">
-        <DecoBoard />
-        <PrimaryButton className="flex" onClick={() => navigate('/game')}><Contact size={25} className="mt-1 mr-5 text-amber-300" />Play friend</PrimaryButton>
-      </PrimaryContainer>
-      <PrimaryContainer className="flex flex-col items-center justify-center gap-5 hover:scale-[1.05] duration-200">
-        <DecoBoard />
-        <PrimaryButton className="flex" onClick={() => navigate('/game')}><ChessQueen size={25} className="mt-1 mr-5 text-amber-300" />Play random</PrimaryButton>
-      </PrimaryContainer>
-      <PrimaryContainer className="flex flex-col items-center justify-center gap-5 hover:scale-[1.05] duration-200">
-        <DecoBoard />
-        <PrimaryButton className="flex" onClick={() => navigate('/game')}><Bot size={25} className="mt-1 mr-5 text-amber-300" />Play robot</PrimaryButton>
-      </PrimaryContainer>
+
+      <div className={`${wrapperClass}`}>
+        <PrimaryContainer className={containerClass}>
+          <DecoBoard />
+          <PrimaryButton className="flex" onClick={() => navigate('/game')}>
+            <Contact size={25} className="mt-1 mr-5 text-amber-300" /> Play friend
+          </PrimaryButton>
+        </PrimaryContainer>
+      </div>
+
+      <div className={`${wrapperClass} delay-150`}>
+        <PrimaryContainer className={containerClass}>
+          <DecoBoard />
+          <PrimaryButton className="flex" onClick={() => navigate('/game')}>
+            <ChessQueen size={25} className="mt-1 mr-5 text-amber-300" /> Play random
+          </PrimaryButton>
+        </PrimaryContainer>
+      </div>
+
+      <div className={`${wrapperClass} delay-300`}>
+        <PrimaryContainer className={containerClass}>
+          <DecoBoard />
+          <PrimaryButton className="flex" onClick={() => navigate('/game')}>
+            <Bot size={25} className="mt-1 mr-5 text-amber-300" /> Play robot
+          </PrimaryButton>
+        </PrimaryContainer>
+      </div>
+
     </div>
   );
 }
