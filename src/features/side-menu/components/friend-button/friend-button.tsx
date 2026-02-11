e11fed93cfecec98ef261fbf9b339c6649b0ee65
fogelt <fogeltt@gmail.com>
2026-02-11 19:33:43 +0100

    Added challenge-button in friendslist

--- src/features/side-menu/components/friend-button/friend-button.tsx
+++ src/features/side-menu/components/friend-button/friend-button.tsx
@@ -1,17 +1,42 @@
-import { GripVertical, Circle } from "lucide-react";
+import { ChevronRight, ChevronDown, Swords } from "lucide-react";
+import { useState } from "react";
+
 
 interface FriendButtonProps {
   children?: string;
+}
 
+const handleChallenge = (username: string) => {
+  console.log("challenged " + username)
 }
 
 export function FriendButton({ children }: FriendButtonProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="pt-2 flex space-x-1 items-center font-light text-sm uppercase tracking-[0.1em]">
-      <button>
-        <GripVertical size={16} className="mt-[1px]" />
-      </button>
-      <span className="">{children}</span>
+    <div className="flex flex-col w-full">
+      <div className="pt-2 flex space-x-1 items-center font-light text-sm uppercase tracking-[0.1em]">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="hover:bg-white/10 p-1 rounded-md transition-colors"
+        >
+          {isOpen ? (
+            <ChevronDown size={16} className="mt-[1px]" />
+          ) : (
+            <ChevronRight size={16} className="mt-[1px]" />
+          )}
+        </button>
+        <span className="cursor-default">{children}</span>
+      </div>
+
+      {isOpen && (
+        <div className="ml-6 mt-1 flex flex-col space-y-2 normal-case tracking-normal">
+          <button className="text-left text-sm flex flex-row hover:bg-white/10 rounded-md w-fit p-1 transition-colors"
+            onClick={() => handleChallenge(children!)}>
+            <Swords size={16} className="mt-[3px] mr-1 text-amber-300" /> Challenge
+          </button>
+        </div>
+      )}
     </div>
   );
 }
